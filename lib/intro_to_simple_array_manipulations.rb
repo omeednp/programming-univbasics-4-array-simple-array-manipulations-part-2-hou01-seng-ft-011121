851c27b02fbca983d7ef796b8d8e2421e0f68931
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-04 06:15:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,10 +14,10 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete
-  
+def using_delete(array, string)
+  array.delete(string)
 end
 
-def using_delete_at
+def using_delete_at(array, integer)
   
 end
