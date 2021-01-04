da971f22f27dd61d473c6321a1f7bb0393a2c675
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-04 06:12:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,20 +1,23 @@
-def using_concat
-  
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_insert
-  
+def using_insert(array, element)
+  array.insert(element)
 end
 
-def using_uniq
-  
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten
-  
+def using_flatten(array)
+  array.flatten
 end
 
 def using_delete
   
 end
 
+def using_delete_at
+  
+end
